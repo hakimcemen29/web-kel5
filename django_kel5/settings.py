6bcd8759c7519c9f9f9b57b47b37c9ffe47d3227
hakimcemen29 <mhmd.hakim29@gmail.com>
2025-07-03 17:23:54 +0700

    Tambahkan STATIC_ROOT di settings.py untuk collectstatic

--- django_kel5/settings.py
+++ django_kel5/settings.py
@@ -18,7 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
